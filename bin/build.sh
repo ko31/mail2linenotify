d882d8d5b653fab1af197c791004b83423c976e3
Ko Takagi <knockoutmarch@gmail.com>
2020-11-09 12:14:52 +0900

    :wrench: add release action

---
+++ bin/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+rm -rf ./.git
+rm -rf ./.github
+rm -rf ./.gitignore
+rm -rf ./bin
+rm -rf ./composer.lock
+rm -rf ./editorconfig
+rm -rf ./Gruntfile.js
+rm -rf ./package.json
+rm -rf ./package-lock.json
+rm -rf ./phpunit.xml*
+rm -rf ./tests
