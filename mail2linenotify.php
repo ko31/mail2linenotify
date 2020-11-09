2b3ada7fa0ae1b0d7879c4e664fcca8fe005a7a3
Ko Takagi <knockoutmarch@gmail.com>
2020-11-09 12:06:36 +0900

    :+1: update send message

--- mail2linenotify.php
+++ mail2linenotify.php
@@ -2,42 +2,140 @@
 require( "vendor/autoload.php" );
 
 use ZBateson\MailMimeParser\Message;
+use ZBateson\MailMimeParser\Header\HeaderConsts;
 
+/**
+ * Class MailToLineNotify
+ */
 class MailToLineNotify {
+    /**
+     * LINE Notify endpoint URL.
+     */
     const ENDPOINT_URL = 'https://notify-api.line.me/api/notify';
 
+    /**
+     * LINE Notify maximum message length.
+     */
+    const MAX_MESSAGE_LENGTH = 1000;
+
+    /**
+     * LINE Notify access token.
+     *
+     * @var string
+     */
     private $access_token;
 
+    /**
+     * Constructor.
+     *
+     * @param $access_token
+     */
     public function __construct( $access_token ) {
         $this->access_token = $access_token;
         $this->run();
     }
 
+    /**
+     * Run proccess.
+     */
     public function run() {
         $content = $this->parseMailContent();
         if ( empty( $content ) ) {
             return;
         }
-        $this->notify( $content );
+        $this->send( $content );
     }
 
+    /**
+     * Get standard input.
+     *
+     * @return false|string
+     */
     public function getStdin() {
         return file_get_contents( "php://stdin" );
     }
 
+    /**
+     * Parse email content.
+     *
+     * @return string
+     */
     public function parseMailContent() {
         $stdin = $this->getStdin();
         if ( empty( $stdin ) ) {
             return '';
         }
+
         $message = Message::from( $stdin );
         if ( empty( $message ) ) {
             return '';
         }
 
-        return $message->getTextContent();
+        return sprintf( "\n[メール件名]\n%s\n\n[メール本文]\n%s", $message->getHeaderValue( HeaderConsts::SUBJECT ), $message->getTextContent() );
     }
 
+    /**
+     * Send message to LINE Notify
+     *
+     * @param $content
+     */
+    public function send( $content ) {
+        $messages = $this->splitMessage( $content );
+        foreach ( $messages as $message ) {
+            $this->notify( $message );
+        }
+    }
+
+    /**
+     * Split message text.
+     *
+     * @param $message
+     *
+     * @return array
+     */
+    public function splitMessage( $message ) {
+        $length = self::MAX_MESSAGE_LENGTH;
+
+        // Split message text
+        $split = [];
+        $i     = 0;
+        while ( true ) {
+            // If the text to be split is shorter than length, it breaks.
+            if ( mb_strlen( mb_substr( $message, $i ) ) <= $length ) {
+                $split[] = mb_substr( $message, $i );
+                break;
+            }
+
+            // Get the position to split
+            $substr    = mb_substr( $message, $i, $length );
+            $split_pos = mb_strrpos( $substr, "\n" );
+            if ( $split_pos === false ) {
+                // If no line break code
+                $split_pos = $length;
+            } else if ( $split_pos === 0 ) {
+                // If a leading character is line break code
+                $split_pos = $length;
+                // Skip a leading character
+                $i ++;
+            } else {
+                $split_pos ++;
+            }
+
+            $split[] = mb_substr( $message, $i, $split_pos );
+
+            $i += $split_pos;
+        }
+
+        return $split;
+    }
+
+    /**
+     * Send LINE Notify
+     *
+     * @param $message
+     *
+     * @return bool|string
+     */
     public function notify( $message ) {
         $headers   = [
             'Authorization: Bearer ' . $this->access_token,
@@ -63,6 +161,3 @@ if ( empty( $argv[1] ) ) {
 
 // Send message from mail to LINE Nofify.
 $mail2linenotify = new MailToLineNotify( $argv[1] );
-
-echo 'done';
-
